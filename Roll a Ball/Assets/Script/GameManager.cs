4306da9a3fe25f0621be0d90a6da16c74c8b5121
jsuk10 <jsuk10@naver.com>
2021-04-01 19:36:26 +0900

    feat : Roll a ball

---
+++ Roll a Ball/Assets/Script/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+enum SceneName { 
+   SampleScene
+}
+
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Hole[] holes;
+    [SerializeField] private GameObject[] clearShowObject;
+    [SerializeField] private Text nowTime;
+    [SerializeField] private Text clearTime;
+    [SerializeField] private SceneName resetScene;
+    private bool clear = false;
+    private float time = 0f;
+    private int min = 0;
+
+    private void Start()
+    {
+        Screen.SetResolution(1080, 1920, true);
+        for (int i = 0; i < clearShowObject.Length; i++)
+        {
+            clearShowObject[i].SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        if (!clear)
+        {
+            time += Time.deltaTime;
+            if(time > 60f)
+            {
+                min++;
+                time = 0f;
+            }
+            nowTime.text = string.Format("{0:D2} : {1:D2}", min, (int)time);
+        }
+        for (int i = 0; i < holes.Length; i++)
+        {
+            if (holes[i].Ballin == false)
+            {
+                return;
+            }
+        }
+        Clear();
+        return;
+    }
+
+    private void Clear() {
+        clear = true;
+        for (int i = 0; i < clearShowObject.Length; i++) {
+            clearShowObject[i].SetActive(true);
+        }
+
+        clearTime.text = "Clear!! \n Your Clear Time\n" + time.ToString("00:00");
+    }
+
+    public void GameReset() {
+        SceneManager.LoadScene(resetScene.ToString());
+    }
+}
---
+++ Roll a Ball/Assets/Script/Hole.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum TagName { 
+    BlueBall,
+    RedBall,
+    GreenBall
+}
+
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField]
+    private TagName tagName;
+    private bool ballIn = false;
+    public bool Ballin {
+        get { return ballIn; }
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ballIn = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+
+        Vector3 driction = (transform.position - other.transform.position).normalized;
+        if (other.gameObject.tag == tagName.ToString())
+        {
+            rb.velocity *= 0.9f;
+            rb.AddForce(driction * rb.mass * 20f);
+        }
+        else { 
+            rb.AddForce(-driction * rb.mass * 80f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ballIn = false;
+    }
+}
